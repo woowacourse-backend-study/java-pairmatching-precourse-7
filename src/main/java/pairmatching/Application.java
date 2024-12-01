270877434ffefc6ae6654ab94fdf9d2431cfed77
Beomseok Noh <prodigy0831@naver.com>
2024-12-01 20:04:03 +0900

    newstart

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -9,5 +9,6 @@ public class Application {
     public static void main(String[] args) {
         MainController mainController = new MainController(InputView.getInstance(), OutputView.getInstance());
         mainController.process();
+
     }
 }
