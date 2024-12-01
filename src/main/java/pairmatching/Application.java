e0188cde9dabb9a664e20cf1ecf49657191e39e4
minSsan <msp1125@hanyang.ac.kr>
2024-12-01 18:07:02 +0900

    feat(Application): 어플리케이션 클래스 연결

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,17 @@
 package pairmatching;
 
+import pairmatching.controller.MainController;
+import pairmatching.service.PairMatchingService;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        MainController mainController = new MainController(
+                new InputView(),
+                new OutputView(),
+                new PairMatchingService()
+        );
+        mainController.run();
     }
 }
